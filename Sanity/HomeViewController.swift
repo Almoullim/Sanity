5b9d8c88273c58ee1c4981207a88f01ca1a05fab
Ali Almoullim <ali.almoullim@gmail.com>
2019-03-30 21:47:59 +0300

    Add second story board

---
+++ Sanity/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Sanity
+//
+//  Created by Ali on 3/30/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Set the stuatus bar content to white
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
