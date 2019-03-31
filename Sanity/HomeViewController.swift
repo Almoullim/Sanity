04da01307ccabcc7a0b097a3966559f0942dca6f
Ali Almoullim <ali.almoullim@gmail.com>
2019-03-31 19:38:51 +0300

    Cleanup file

--- Sanity/HomeViewController.swift
+++ Sanity/HomeViewController.swift
@@ -15,20 +15,4 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-    // Set the stuatus bar content to white
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
