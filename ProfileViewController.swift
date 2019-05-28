dee42d3606120e0484306bd26df15656475074e9
Ali Almoullim <ali.almoullim@gmail.com>
2019-05-28 09:36:27 +0300

    Create profile screen

---
+++ ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Sanity
+//
+//  Created by Ali on 5/28/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
