12ddf18db18d98663f081f8714c9004c9134798c
Ali Almoullim <ali.almoullim@gmail.com>
2019-05-28 13:08:53 +0300

    create edit profile page

---
+++ EditProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditProfileViewController.swift
+//  Sanity
+//
+//  Created by Ali on 5/28/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
