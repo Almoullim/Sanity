c9054c0a1b12459a40f5860bfff7d0f42293985c
Ali Hubail <201501533@student.polytechnic.bh>
2019-06-15 16:41:04 +0300

    Questions

---
+++ Question.swift
@@ -0,0 +1,24 @@
+//
+//  Question.swift
+//  Sanity
+//
+//  Created by Ali Hubail on 6/15/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
