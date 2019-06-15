ba93373628945c8ae62b38afebfdebd0bb49f88c
Ali Hubail <201501533@student.polytechnic.bh>
2019-06-15 10:33:50 +0300

    Update Controller

---
+++ AdminContollers/UsersListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersListViewController.swift
+//  Sanity
+//
+//  Created by Ali Hubail on 6/15/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+
+class UsersListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UserCellDelegate  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
