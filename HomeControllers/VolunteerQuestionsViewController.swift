a5b4b7674d7f7a3c3ee77307874df88ac28bf440
Ali Almoullim <ali.almoullim@gmail.com>
2019-06-23 23:21:40 +0300

    update spacing

--- HomeControllers/VolunteerQuestionsViewController.swift
+++ HomeControllers/VolunteerQuestionsViewController.swift
@@ -18,13 +18,25 @@ class VolunteerQuestionsViewController: UITableViewController, QuestionCellDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//
-//        let q1 = Question2(id: "1", title: "First Question", userAnswer: true, answerShouldBe: true)
-//        let q2 = Question2(id: "2", title: "Second Question", userAnswer: true, answerShouldBe: true)
-//        let q3 = Question2(id: "3", title: "Third Question", userAnswer: true, answerShouldBe: true)
-//        let q4 = Question2(id: "4", title: "Fourth Question", userAnswer: true, answerShouldBe: true)
-//
-//        questions = [q1,q2,q3,q4]
+        
+        let a1 = Answer(text: "Best", type: .best)
+        let a2 = Answer(text: "Good", type: .good)
+        let a3 = Answer(text: "Bad", type: .bad)
+        let a4 = Answer(text: "Worst", type: .worst)
+        let answers = [a1,a2,a3,a4]
+        
+        let a12 = Answer(text: "Yes", type: .best)
+        let a22 = Answer(text: "No", type: .good)
+        let a32 = Answer(text: "MAYBE", type: .bad)
+        let a42 = Answer(text: "SURE", type: .worst)
+        let answers2 = [a12,a22,a32,a42]
+        
+        let q1 = Question(ID: "1", text: "First Question?", usertype: .Volunteer, answers: answers)
+        let q2 = Question(ID: "2", text: "Second Question?", usertype: .Volunteer, answers: answers2)
+        let q3 = Question(ID: "3", text: "Third Question?", usertype: .Volunteer, answers: answers)
+        let q4 = Question(ID: "4", text: "Fourth Question?", usertype: .Volunteer, answers: answers2)
+
+        questions = [q1,q2,q3,q4]
         
     }
     
@@ -52,9 +64,15 @@ class VolunteerQuestionsViewController: UITableViewController, QuestionCellDeleg
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell =  tableView.dequeueReusableCell(withIdentifier: "QuestionCell") as? QuestionCell else { return UITableViewCell() }
-        print("Building Cell")
-//        cell.questionTitle.text = questions[indexPath.row].title
-//        cell.questionId = questions[indexPath.row].id
+        
+        cell.questionTitle.text = questions[indexPath.row].text
+        cell.questionId = questions[indexPath.row].ID
+        
+        cell.questionAnswerController.setTitle(questions[indexPath.row].answers[0].text, forSegmentAt: 0)
+        cell.questionAnswerController.setTitle(questions[indexPath.row].answers[1].text, forSegmentAt: 1)
+        cell.questionAnswerController.setTitle(questions[indexPath.row].answers[2].text, forSegmentAt: 2)
+        cell.questionAnswerController.setTitle(questions[indexPath.row].answers[3].text, forSegmentAt: 3)
+        
         cell.delegate = self
         
         return cell
