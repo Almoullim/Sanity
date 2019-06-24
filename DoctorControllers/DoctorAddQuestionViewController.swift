01b0a31a44921b99a1752397bf20530e3ecbb48f
Ali Hubail <201501533@student.polytechnic.bh>
2019-06-25 02:19:33 +0300

    doctor Questions

---
+++ DoctorControllers/DoctorAddQuestionViewController.swift
@@ -0,0 +1,169 @@
+//
+//  DoctorAddQuestionViewController.swift
+//  Sanity
+//
+//  Created by Ali Hubail on 6/25/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class DoctorAddQuestionViewController: UITableViewController {
+
+    
+    // pass info
+    var selectedQuestion: String?
+    var userType: String?
+    
+    
+    // outlet
+    @IBOutlet weak var QuestionInput: UITextView!
+    @IBOutlet weak var BestAnswer: UITextField!
+    @IBOutlet weak var goodAnswerInput: UITextField!
+    @IBOutlet weak var badAnswerInput: UITextField!
+    @IBOutlet weak var worstAnswerInput: UITextField!
+    @IBOutlet weak var AddEditButton: UIBarButtonItem!
+    
+    // firebase connection
+    var db: Firestore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // firebase api code
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // get user info if there is passed information
+        if let question = self.selectedQuestion {
+            let docRef = db.collection("qustions").document(question)
+            
+            docRef.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: \(dataDescription)")
+                    if let data = document.data() {
+                        self.QuestionInput.text = data["question"] as? String
+                        if let answers = data["answers"] as? [String: String] {
+                            self.BestAnswer.text = answers["best"]
+                            self.goodAnswerInput.text = answers["good"]
+                            self.badAnswerInput.text = answers["bad"]
+                            self.worstAnswerInput.text = answers["worst"]
+                        }
+                    }
+                } else {
+                    print("Document does not exist")
+                }
+            }
+            
+        }else {
+            // change title and button type if there isn't passed information
+            self.navigationItem.title = "Add Question"
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(AddQuestionTableViewController.AddClicked(_:)))
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 4
+        case 2:
+            if let question = self.selectedQuestion{
+                return 1
+            } else {
+                return 0
+            }
+        default:
+            return 0
+            
+            
+        }
+    }
+    
+    
+    @IBAction func AddClicked(_ sender: Any) {
+        // go back to questions list
+        performSegue(withIdentifier: "doctorquestions", sender: nil)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "doctorquestions" {
+            if sender == nil {
+                if segue.destination is QuestionsTableViewController
+                {
+                    // pass userType back
+                    let view = segue.destination as? QuestionsTableViewController
+                    view?.userType = self.userType
+                }
+                // get data from outlets and insert/ overwrite information to database
+                let docData: [String: Any] = [
+                    "question": self.QuestionInput.text!,
+                    "userType": self.userType!,
+                    "answers": [
+                        "best": self.BestAnswer.text!,
+                        "good": self.goodAnswerInput.text!,
+                        "bad": self.badAnswerInput.text!,
+                        "worst": self.worstAnswerInput.text!
+                    ]
+                ]
+                if let questionID = self.selectedQuestion {
+                    self.db
+                        .collection("qustions")
+                        .document(questionID)
+                        .setData(docData, merge: true)
+                        { err in
+                            if let err = err {
+                                print("Error updating document: \(err)")
+                            } else {
+                                print("Question updated!")
+                            }
+                    }
+                } else {
+                    self.db
+                        .collection("qustions")
+                        .document()
+                        .setData(docData, merge: true)
+                        { err in
+                            if let err = err {
+                                print("Error updating document: \(err)")
+                            } else {
+                                print("Question updated!")
+                            }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // assign cell article to variable
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath {
+        case [2, 0]:
+            if let questionID = self.selectedQuestion {
+                db.collection("qustions").document(questionID).delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        print("Document successfully removed!")
+                    }
+                }
+                performSegue(withIdentifier: "doctorquestions", sender: "delete")
+            }
+        default:
+            break
+        }
+        
+        
+    }
+}
---
+++ DoctorControllers/DoctorQuestionsViewController.swift
@@ -0,0 +1,175 @@
+//
+//  DoctorQuestionsViewController.swift
+//  Sanity
+//
+//  Created by Ali Hubail on 6/25/19.
+//  Copyright © 2019 Almoullim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class DoctorQuestionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, AdminSettingDelegate  {
+    
+    // Google Firestore connection
+    var db: Firestore!
+    var storage: Storage!
+    
+    // outlet
+    @IBOutlet weak var QuestionsTable: UITableView!
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    // pass info
+    var selectedQuestion: String?
+    var userType: String?
+    
+    var questions: [Question] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // firebase api coce
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+        storage = Storage.storage()
+        
+        // load help seeker question(default fillter)
+        loadQuestions(userType: "help-seeker", searchText: nil)
+        
+        // dalegate
+        SearchBar.delegate = self
+        QuestionsTable.delegate = self
+        QuestionsTable.dataSource = self
+        
+    }
+    
+    
+    func segueWith(ID: String) {
+        selectedQuestion = ID
+        requestedButtonClicked(self)
+    }
+    @IBAction func requestedButtonClicked(_ sender: Any) {
+        performSegue(withIdentifier: "EditQuestion", sender: nil)
+    }
+    @IBAction func addButtonClicked(_ sender: Any) {
+        performSegue(withIdentifier: "AddQuestion", sender: nil)
+    }
+    
+    func searchBarSearchButtonClicked( _ searchBar: UISearchBar) {
+        // get search text and reload sessions
+        if let text = searchBar.text {
+            questions = []
+            self.QuestionsTable.reloadData()
+            
+            loadQuestions(userType: "help-seeker", searchText: text)
+        }
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // discharge search and reload all sessions
+        searchBar.text = nil
+        questions = []
+        self.QuestionsTable.reloadData()
+        loadQuestions(userType: "help-seeker", searchText: nil)
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // get questions count and assign to rows count
+        return questions.count
+    }
+    
+    func loadQuestions(userType: String, searchText: String?) {
+        // set collection [table]
+        let collection = db.collection("qustions")
+        // set query
+        var query = collection.whereField("userType", isEqualTo: userType)
+        
+        if searchText != nil {
+            // set search query
+            query = collection.order(by: "name").start(at: [searchText!]).end(at: [searchText! + "\u{f8ff}"])
+        }
+        // get document
+        query.getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                self.addQuestion(querySnapshot!.documents)
+            }
+        }
+    }
+    
+    func addQuestion(_ documents: [QueryDocumentSnapshot]) {
+        // get each documnt info and assign to new question
+        for document in documents {
+            // Get the volunteer name of the document
+            let questionID = document.documentID
+            self.selectedQuestion = questionID
+            let question = document.data()["question"] as! String
+            let userType = document.data()["userType"] as! String
+            var userTypeCase: Usertype
+                userTypeCase = .helpSeeker
+            
+            self.userType = userType
+            
+            let qusetion = Question(ID: questionID, text: question, usertype: userTypeCase, answers: [Answer(text: "", rate: .good)], userAnswer: nil)
+            
+            // add new quetion to the collection
+            self.questions.append(qusetion)
+        }
+        
+        QuestionsTable.reloadData()
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // assign quetion info to cell
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "QuestionCell") as? AdminSettingCellTableView
+        
+        cell?.qustion.text = self.questions[indexPath.row].text
+                    cell?.userType.text = "help-seeker"
+        cell?.ID = self.questions[indexPath.row].ID
+        cell?.delegate = self
+        
+        
+        return cell!
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // pass info to next view
+        if segue.identifier == "EditQuestion" {
+            if segue.destination is AddQuestionTableViewController
+            {
+                let view = segue.destination as? AddQuestionTableViewController
+                view?.selectedQuestion = self.selectedQuestion
+                view?.userType = self.userType
+            }
+        } else if segue.identifier == "AddQuestion" {
+            if segue.destination is AddQuestionTableViewController
+            {
+                let view = segue.destination as? AddQuestionTableViewController
+                
+                view?.userType = "help-seeker"
+            }
+        }
+    }
+    
+    
+    @IBAction func unwindToDoctorQustions(unwindSegue: UIStoryboardSegue) {
+        // unwind from add/edit question
+        questions = []
+        loadQuestions(userType: "help-seeker", searchText: nil)
+        self.QuestionsTable.reloadData()
+    }
+    
+    
+    
+
+}
